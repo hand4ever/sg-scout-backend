0095c06baaa4c170bd47967a6b19095f91b4af0c
panlong <panlonggl@vip.163.com>
2026-09-02 22:33:34 +0800

    fix: add json tags to demo entities for lowercase keys

--- entity/demo/demo.go
+++ entity/demo/demo.go
@@ -2,10 +2,10 @@ package demo
 
 // Filter binds multi-value query parameters for /demo/search.
 type Filter struct {
-	Tags []string `query:"tag"`
+	Tags []string `query:"tag" json:"tags"`
 }
 
 // EchoReq binds a path parameter for /demo/echo/:str.
 type EchoReq struct {
-	Str string `param:"str"`
+	Str string `param:"str" json:"str"`
 }
